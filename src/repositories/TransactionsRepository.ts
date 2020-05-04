2d1cfb8ec151d9899e282b5774d7301c035c75e6
masvi <jeanmasvi@gmail.com>
2020-05-04 08:01:07 -0300

    listing and creating transations. Validation on balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,20 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TransactionWithBalanceDTO {
+  transactions: Array<Transaction>;
+  balance: Balance;
 }
 
 class TransactionsRepository {
@@ -13,16 +24,39 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): TransactionWithBalanceDTO {
+    const { income, outcome, total } = this.getBalance();
+    const obj = {
+      transactions: this.transactions,
+      balance: {
+        income,
+        outcome,
+        total,
+      },
+    };
+
+    return obj;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .filter(item => item.type === 'income')
+      .reduce((acc, cur) => acc + Number(cur.value), 0);
+
+    const outcome = this.transactions
+      .filter(item => item.type === 'outcome')
+      .reduce((acc, cur) => acc + Number(cur.value), 0);
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
